627c17d23ce7a8d5a9510c389ea0aa6d4e99bc7e
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-04-29 23:50:04 -0700

    Updates

---
+++ precache-manifest.0a6dcffc81e78cb29b2ab826bf8709ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58478a61a73004c270a727d47bcc95dd",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "240315a9ba13c1eef37f",
+    "url": "/clickygame/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4686526d17a9506015a6",
+    "url": "/clickygame/static/css/main.010a03a0.chunk.css"
+  },
+  {
+    "revision": "240315a9ba13c1eef37f",
+    "url": "/clickygame/static/js/2.a43fbd7a.chunk.js"
+  },
+  {
+    "revision": "4686526d17a9506015a6",
+    "url": "/clickygame/static/js/main.d99621eb.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
